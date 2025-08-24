8b14003a58ca3142275bc014a97fc8d4c724fc7f
Antonio Cicero Azevedo <azevedoantoniocicero@gmail.com>
2025-08-24 01:41:38 -0300

    definindo temas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
-module.exports = {
+import tailwindTypography from '@tailwindcss/typography';
+
+export default {
   darkMode: 'class',
   content: ['./src/**/*.md', './src/**/*.html', './src/_includes/**/*.njk'],
-  plugins: [
-    require('@tailwindcss/typography')
-  ],
+  plugins: [tailwindTypography],
+  theme: {
+    extend: {
+      colors: {
+        background: {
+          dark: "#1e1e2e",
+          light: "#eff1f5"
+        },
+        text: {
+          dark: "#eff1f5",
+          light: "#1e1e2e"
+
+        }
+      }
+    } 
+  },
 }
